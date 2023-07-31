459c6c144bde563b59ef8218410cc7e624396886
Ricardo <2025944194@qq.com>
2023-07-31 18:23:29 +0800

    change

--- manager/public/config.js
+++ manager/public/config.js
@@ -3,25 +3,25 @@ var BASE = {
      * @description api请求基础路径
      */
     API_DEV: {
-      common: "http://47.95.116.52:8890",
-      buyer: "http://47.95.116.52:8888",
-      seller: "http://47.95.116.52:8889",
-      manager: "http://47.95.116.52:8887"
+      common: "http://175.27.169.100:8890",
+      buyer: "http://175.27.169.100:8888",
+      seller: "http://175.27.169.100:8889",
+      manager: "http://175.27.169.100:8887"
     },
     API_PROD: {
-      common: "http://47.95.116.52:8890",
-      buyer: "http://47.95.116.52:8888",
-      seller: "http://47.95.116.52:8889",
-      manager: "http://47.95.116.52:8887"
+      common: "http://175.27.169.100:8890",
+      buyer: "http://175.27.169.100:8888",
+      seller: "http://175.27.169.100:8889",
+      manager: "http://175.27.169.100:8887"
     },
     /**
      * @description // 跳转买家端地址 pc端
      */
-    PC_URL: "http://47.95.116.52",
+    PC_URL: "http://175.27.169.100",
     /**
      * @description  // 跳转买家端地址 wap端
      */
-    WAP_URL: "http://47.95.116.52:10004",
+    WAP_URL: "http://175.27.169.100:10004",
     /**
      *  @description api请求基础路径前缀
      */
