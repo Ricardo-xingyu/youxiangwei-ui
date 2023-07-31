bedebf7a6870ad26e20b8706fd795c484bae21e7
Ricardo <2025944194@qq.com>
2023-07-31 18:21:54 +0800

    change

--- buyer/public/config.js
+++ buyer/public/config.js
@@ -3,15 +3,15 @@ var BASE = {
      * @description api请求基础路径
      */
     API_DEV: {
-      common: "http://47.95.116.52:8890",
-      buyer: "http://47.95.116.52:8888",
-      seller: "http://47.95.116.52:8889",
-      manager: "http://47.95.116.52:8887"
+      common: "http://175.27.169.100:8890",
+      buyer: "http://175.27.169.100:8888",
+      seller: "http://175.27.169.100:8889",
+      manager: "http://175.27.169.100:8887"
     },
     API_PROD: {
-      common: "http://47.95.116.52:8890",
-      buyer: "http://47.95.116.52:8888",
-      seller: "http://47.95.116.52:8889",
-      manager: "http://47.95.116.52:8887"
+      common: "http://175.27.169.100:8890",
+      buyer: "http://175.27.169.100:8888",
+      seller: "http://175.27.169.100:8889",
+      manager: "http://175.27.169.100:8887"
     },
   };
